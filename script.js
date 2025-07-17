448355072682ce2c270ead1121eb6eeb6fb40974
Marlonn Gillet <gillet.marlonn56@icloud.com>
2025-07-17 18:40:33 +0200

    change

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 
-  document.querySelector(".burger").addEventListener("click", function () {
-    document.querySelector(".nav-menu").classList.toggle("active");
-  });
-  document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("DOMContentLoaded", () => {
   const burger = document.querySelector(".burger");
   const navMenu = document.querySelector(".nav-menu");
-  if (burger && navMenu) {
-    burger.addEventListener("click", () => {
-      navMenu.classList.toggle("active");
-    });
-  }
-});
 
+  burger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+});
