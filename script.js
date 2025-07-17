580b858b08092e6ec478cb76c1ecf7d356bc621e
Marlonn Gillet <gillet.marlonn56@icloud.com>
2025-07-17 18:35:27 +0200

    ajout js

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
   document.querySelector(".burger").addEventListener("click", function () {
     document.querySelector(".nav-menu").classList.toggle("active");
   });
+  document.addEventListener("DOMContentLoaded", function () {
+  const burger = document.querySelector(".burger");
+  const navMenu = document.querySelector(".nav-menu");
+  if (burger && navMenu) {
+    burger.addEventListener("click", () => {
+      navMenu.classList.toggle("active");
+    });
+  }
+});
+
